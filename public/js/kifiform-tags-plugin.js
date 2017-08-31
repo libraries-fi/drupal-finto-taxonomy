c4e1402ae222b431321467b6d06b006b7fb6aa5c
Samu Juvonen <samu.juvonen@kirjastot.fi>
2017-08-31 19:21:30 +0300

    Use native data access to always get values as strings

--- public/js/kifiform-tags-plugin.js
+++ public/js/kifiform-tags-plugin.js
@@ -4,16 +4,14 @@
   $("input.form-autocomplete")
     .once("finto-taxonomy-tag-insert")
     .on("kififormtaginsert", function(event, ui) {
-      var tids = ui.autocomplete.data("finto_taxonomy-terms").split(/,/);
+      var tids = ui.autocomplete[0].dataset.finto_taxonomyTerms.split(/,/);
 
       if (ui.item.value.match(/\(p.*?\)$/)) {
         ui.tag.addClass("finto-taxonomy-tag");
-        // console.log("FINTO TAG", ui.item.value);
       } else {
         var match = /\((\d+)\)$/.exec(ui.item.value);
 
         if (match && tids.indexOf(match[1]) != -1) {
-          console.log("MOI", ui.item.value);
           ui.tag.addClass("finto-taxonomy-tag");
         }
       }
